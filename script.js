aeea503e873f47a3748747625559f001ed45425d
Sanju31 <87665546+Sanju31@users.noreply.github.com>
2026-08-27 17:18:55 -0500

    Update script.js for firebase

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
 /* =====================================================
+FIREBASE
+===================================================== */
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    doc,
+    setDoc,
+    addDoc,
+    updateDoc,
+    arrayUnion
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCRt1cjbvGe2bkes4VWBPx2QCStUKGWyUs",
+
+    authDomain: "park-crm-bb00a.firebaseapp.com",
+
+    projectId: "park-crm-bb00a",
+
+    storageBucket: "park-crm-bb00a.firebasestorage.app",
+
+    messagingSenderId: "667776961583",
+
+    appId: "1:667776961583:web:295fb1083dacc869a88ada"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+console.log("🔥 Firebase connected:", app.name);
+
+/* =====================================================
 USER ACCOUNTS
 ===================================================== */
 
